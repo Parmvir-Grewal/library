253a560e10ea522e2bdcf7aba23d5ca8455baf5b
Parmvir Grewal <parmg100@gmail.com>
2021-08-03 22:49:41 -0400

    Create basic library display

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
 let myLibrary = [
     {
-      title: "The Hobbit",
-      author: "J.R.R Tolkien",
-      pages: 295,
-      read: false
+        title: "The Hobbit",
+        author: "J.R.R Tolkien",
+        pages: 295,
+        read: false
     }
-  ];
-  
-  function Book(title, author, numPages, read) {
-      this.title = title;
-      this.author = author;
-      this.numPages = numPages;
-      this.read = read;
-      this.info = function() {
-        return `${title} by ${author}, ${numPages}, ${read}`;
-      }
-    }
-  
-  function addBookToLibrary(title, author, numPages, read) {
-      const newBook = new Book(title, author, numPages, read);
-      myLibrary.push(newBook);
-  }
-  
-  function viewAllBooks() {
+];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+
+function viewAllBooks() {
+    let container = document.querySelector('#container');
     for (let i = 0; i < myLibrary.length; i++) {
-      console.log(myLibrary[i].title);
+        let book = document.createElement('div');
+        let bookTitle = document.createElement('H2');
+        let bookDescription = document.createElement('p');
+        book.classList.add("bookContainer");
+        bookTitle.innerHTML = myLibrary[i].title;
+        bookDescription.innerHTML = `By: ${myLibrary[i].author} \nNumber of pages: ${myLibrary[i].pages} \nRead: ${myLibrary[i].read}`;
+        container.appendChild(book);
+        book.appendChild(bookTitle);
+        book.appendChild(bookDescription);
     }
-  }
+}
